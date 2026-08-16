3563cec4fcc024c634f47682acb7f28045bb71cc
Aditya291207 <25BCS12248@cuchd.in>
2026-08-16 16:48:32 +0530

    Time: 4 ms (20.55%) | Memory: 63 MB (11.22%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0;
+        int r=height.size()-1;
+        int area=0;
+        while(l<r){
+            int curr_area=min(height[l],height[r])*(r-l);
+            if(height[l]<height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+            area=max(area,curr_area);
+        }
+        return area;
+    }
+};
