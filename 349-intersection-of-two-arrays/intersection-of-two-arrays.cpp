93a99a42ea5159e2702136dc7ad253ec24a675d9
Aditya291207 <25BCS12248@cuchd.in>
2026-08-20 10:41:16 +0530

    Time: 7 ms (10.28%) | Memory: 14.3 MB (57.92%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        set<int>s(nums1.begin(),nums1.end());
+        for(int i:nums2){
+            if(s.find(i) != s.end()) {
+                ans.push_back(i);
+                s.erase(i);
+            }
+        }
+        return ans;
+    }
+};
