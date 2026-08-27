a1325bba8c8f5f58ffc1b6d998744b64fb055346
Aditya291207 <25BCS12248@cuchd.in>
2026-08-27 12:05:22 +0530

    Time: 4 ms (86.92%) | Memory: 25.5 MB (51.31%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>count(26,0);
+        for(char ch:word){
+            count[ch-'a']++;
+        }
+    sort(count.begin(),count.end(),greater<int>());
+int ans =0;
+       for(int i=0;i<26;i++){
+            ans=ans+count[i]*(i/8+1);
+        }
+        return ans; 
+    }
+};
