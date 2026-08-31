269731e466c142849de02274cd7333fb044d9d62
Aditya291207 <25BCS12248@cuchd.in>
2026-08-31 10:55:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.38%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -3,10 +3,8 @@ public:
     int mySqrt(int x) {
         long long low = 1;
         long long high = x;
-
         while (low <= high) {
             long long mid = low + (high - low) / 2;
-
             if (mid * mid <= x) {
                 low = mid + 1;
             } 
@@ -14,7 +12,6 @@ public:
                 high = mid - 1;
             }
         }
-
         return high;
     }
 };
