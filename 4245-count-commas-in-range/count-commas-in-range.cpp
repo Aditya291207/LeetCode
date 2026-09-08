4c45a778f806e5ab2e7f9a2d68815281b000fd33
Aditya291207 <25BCS12248@cuchd.in>
2026-09-08 09:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.38%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int count=0;
+        if(n<=999){
+            return 0;
+        }
+        while(n>999){
+            count++;
+            n--;
+        }
+        return count;
+    }
+};
